61b067c4903d071619dd06353d4a309d2987540d
FernandoZib <fernandozibeuan@gmail.com>
2021-12-07 12:45:07 -0500

    Correccion

--- backend/views/ocupacion/index.php
+++ backend/views/ocupacion/index.php
@@ -5,7 +5,7 @@ use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\search\OcupacionSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $dataProvider yii\data\ActiveDataProvider  */
 /*                                                         */
 
 $this->title = 'Ocupaciones';
